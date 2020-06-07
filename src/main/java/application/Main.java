6cc5e093342c1d2e843e748e160363cfe4502f9c
sderevianko <derevianko.serhii@gmail.com>
2020-06-08 00:59:01 +0300

    Implementing email text notification sending

---
+++ src/main/java/application/Main.java
@@ -0,0 +1,55 @@
+package application;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Main {
+
+    public static void main(String[] args) throws MessagingException, IOException {
+
+        Properties prop = new Properties();
+
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "465");
+        prop.put("mail.smtp.ssl.enable", "true");
+        prop.put("mail.smtp.auth", "true");
+
+        Session session = Session.getInstance(prop, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("seregaserver322@gmail.com", "*****");
+            }
+        });
+        //
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress("seregaserver322@gmail.com"));
+        message.setRecipients(
+            Message.RecipientType.TO, InternetAddress.parse("serya42572@gmail.com"));
+        message.setSubject("Mail Subject");
+
+        String msg = "This is my 6 email using JavaMailer";
+
+        MimeBodyPart mimeBodyPart = new MimeBodyPart();
+        mimeBodyPart.setContent(msg, "text/html");
+
+        Multipart multipart = new MimeMultipart();
+        multipart.addBodyPart(mimeBodyPart);
+
+        message.setContent(multipart);
+
+        Transport.send(message);
+    }
+}
