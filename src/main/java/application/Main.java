cd31c7f261a8fe3d8ffaa81e1ac2455564ea3224
sderevianko <derevianko.serhii@gmail.com>
2020-07-07 16:07:48 +0300

    Correcter smal issues: add logs, do some fields local

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,16 +1,11 @@
 package application;
 
-import org.xml.sax.SAXException;
 import sender.Sender;
-
-import javax.mail.MessagingException;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws MessagingException, IOException, ParserConfigurationException, SAXException {
-        Sender sender = new Sender();
+    public static void main(String[] args) {
+        final Sender sender = new Sender();
 
         sender.addPropertiesOfEmailService();
         sender.addText();
--- src/main/java/sender/Sender.java
+++ src/main/java/sender/Sender.java
@@ -1,7 +1,5 @@
 package sender;
 
-import org.xml.sax.SAXException;
-
 import javax.mail.Authenticator;
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -13,64 +11,89 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
-import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Sender {
 
-    private Properties prop;
     private Message message;
-    private Session session;
     private Multipart multipart;
-    private MimeBodyPart attachmentBodyPartImage;
-    private MimeBodyPart attachmentBodyPartHtml;
+    private static final Logger logger = Logger.getLogger(Sender.class.getName());
 
-    public void addPropertiesOfEmailService() throws MessagingException {
-        prop = new Properties();
+    public void addPropertiesOfEmailService() {
+        final Properties prop = new Properties();
         prop.put("mail.smtp.host", "smtp.gmail.com");
         prop.put("mail.smtp.port", "465");
         prop.put("mail.smtp.ssl.enable", "true");
         prop.put("mail.smtp.auth", "true");
 
-        session = Session.getInstance(prop, new Authenticator() {
+        final Session session = Session.getInstance(prop, new Authenticator() {
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
                 return new PasswordAuthentication("seregaserver322@gmail.com", "*****");
             }
         });
         message = new MimeMessage(session);
-        message.setFrom(new InternetAddress("seregaserver322@gmail.com"));
-        message.setRecipients(
-            Message.RecipientType.TO, InternetAddress.parse("serya42572@gmail.com"));
-        multipart = new MimeMultipart();
+        try {
+            message.setFrom(new InternetAddress("seregaserver322@gmail.com"));
+            message.setRecipients(
+                Message.RecipientType.TO, InternetAddress.parse("serya42572@gmail.com"));
+            multipart = new MimeMultipart();
+        } catch (MessagingException messagingException) {
+            logger.log(Level.SEVERE, "Internet address not found", messagingException);
+        }
     }
 
-    public void addText() throws MessagingException {
-        message.setSubject("Picture");
-        String msg = "This is my last email using JavaMailer";
-        MimeBodyPart mimeBodyPart = new MimeBodyPart();
+    public void addText() {
+        try { message.setSubject("News");
+        final String msg = "News from NASA";
+        final MimeBodyPart mimeBodyPart = new MimeBodyPart();
         mimeBodyPart.setContent(msg, "text/html");
         multipart.addBodyPart(mimeBodyPart);
+        } catch (MessagingException messagingException) {
+            logger.log(Level.SEVERE, "Message not found", messagingException);
+        }
     }
 
-    public void addImage() throws IOException, MessagingException {
-        attachmentBodyPartImage = new MimeBodyPart();
-        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
-        attachmentBodyPartImage.attachFile(new File(classLoader.getResource("image/im.jpg").getFile()));
-        multipart.addBodyPart(attachmentBodyPartImage);
+    public void addImage() {
+        final MimeBodyPart attachmentBodyPartImage = new MimeBodyPart();
+        final ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+        try {
+            try {
+                attachmentBodyPartImage.attachFile(new File(classLoader.getResource("image/im.jpg").getFile()));
+                multipart.addBodyPart(attachmentBodyPartImage);
+            } catch (IOException ioException) {
+                logger.log(Level.SEVERE, "Image not found", ioException);
+            }
+        } catch (MessagingException messagingException) {
+            logger.log(Level.SEVERE, "Message not found", messagingException);
+        }
     }
 
-    public void sendHtmlFile() throws IOException, MessagingException {
-        attachmentBodyPartHtml = new MimeBodyPart();
-        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
-        attachmentBodyPartHtml.attachFile(new File(classLoader.getResource("templates/test.html").getFile()));
-        multipart.addBodyPart(attachmentBodyPartHtml);
+    public void sendHtmlFile() {
+        final MimeBodyPart attachmentBodyPartHtml = new MimeBodyPart();
+        final ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+        try {
+            try {
+                attachmentBodyPartHtml.attachFile(new File(classLoader.getResource("templates/test.html").getFile()));
+                multipart.addBodyPart(attachmentBodyPartHtml);
+            } catch (IOException ioException) {
+                logger.log(Level.SEVERE, "File not found", ioException);
+            }
+        } catch (MessagingException messagingException) {
+            logger.log(Level.SEVERE, "Message not found", messagingException);
+        }
     }
 
-    public void sendEmail() throws MessagingException {
-        message.setContent(multipart);
-        Transport.send(message);
+    public void sendEmail() {
+        try {
+            message.setContent(multipart);
+            Transport.send(message);
+        } catch (MessagingException messagingException) {
+            logger.log(Level.SEVERE, "Message not found", messagingException);
+        }
     }
 }
